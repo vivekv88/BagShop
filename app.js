ce698934fbe2a8efb284ee0e303757e494c9a69d
Vivek-Vishwakarma-8874 <vivekraj2955@gmail.com>
2025-01-09 12:45:36 +0530

    uploading files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000);
---
+++ models/user-model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/bagshop');
+
+const userSchema = mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String,
+    cart: {
+        type: Array,
+        default: []
+    },
+    isadmin: Boolean,
+    orders: {
+        type: Array,
+        default: []
+    },  
+    contact: String,
+    picture: String
+}); 
+
+module.exports = mongoose.model('user', userSchema);
