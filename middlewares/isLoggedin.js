d5c05ed7d1ac69dac2abcedb0640411e41aaef6d
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-04-17 10:53:36 +0000

    m

--- middlewares/isLoggedin.js
+++ middlewares/isLoggedin.js
@@ -1,20 +1,26 @@
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/user-model');
 
-module.exports = async function (req,res,next) {
-    if(!req.cookies.token){
-        req.flash("error","You need to logged in");
-        res.redirect('/');
+module.exports = async function (req, res, next) {
+  if (!req.cookies.token) {
+    req.flash("error", "You need to be logged in");
+    return res.redirect('/'); // ✅ Add return here to prevent further execution
+  }
+
+  try {
+    const decoded = jwt.verify(req.cookies.token, process.env.JWT_KEY);
+    const user = await userModel.findOne({ email: decoded.email }).select("-password");
+
+    if (!user) {
+      req.flash("error", "User not found");
+      return res.redirect('/');
     }
 
-    try {
-        let decoded = jwt.verify(req.cookies.token, process.env.JWT_KEY);
-        let user = await userModel.findOne({email: decoded.email}).select("-password");
-        req.user = user;
-        next();
-    } 
-    catch (error) {
-        req.flash("error","something went wrong");
-        res.redirect('/');
-    }
-}; 
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("JWT Verification Error:", error);
+    req.flash("error", "Something went wrong");
+    return res.redirect('/'); // ✅ Also return here
+  }
+};
